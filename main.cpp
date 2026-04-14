d0f48d6f3d7d6bedddc42b172f60af60edf73747
Bhumikashinge <bhumikashinge1616@gmail.com>
2026-04-14 12:22:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    float balance = 1000, amount;
+
+    do {
+        cout << "\n--- ATM Menu ---\n";
+        cout << "1. Check Balance\n";
+        cout << "2. Deposit\n";
+        cout << "3. Withdraw\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice) {
+            case 1:
+                cout << "Current Balance = " << balance << endl;
+                break;
+
+            case 2:
+                cout << "Enter amount to deposit: ";
+                cin >> amount;
+                balance += amount;
+                cout << "Amount Deposited Successfully!\n";
+                break;
+
+            case 3:
+                cout << "Enter amount to withdraw: ";
+                cin >> amount;
+                if(amount <= balance) {
+                    balance -= amount;
+                    cout << "Please collect your cash\n";
+                } else {
+                    cout << "Insufficient Balance\n";
+                }
+                break;
+
+            case 4:
+                cout << "Thank you for using ATM!\n";
+                break;
+
+            default:
+                cout << "Invalid choice\n";
+        }
+
+    } while(choice != 4);
+
+    return 0;
+}
 
